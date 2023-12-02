c77631cdf29a11f5b867611c5635020898f6a354
Werenfeldt <jwer@itu.dk>
2023-12-02 18:49:40 +0100

    december 2

---
+++ TheBigPyOff/december2/firstP.py
@@ -0,0 +1,45 @@
+red = 12
+green = 13
+blue = 14
+
+cubeList = dict()
+
+sum = 0
+
+def check(redNumber, greenNumber, blueNumber):
+    if(redNumber != None and redNumber > red): return False
+    if(greenNumber != None and greenNumber > green): return False
+    if(blueNumber != None and blueNumber > blue): return False
+    return True
+
+with open("input.txt", 'r') as file:
+    for line in file:
+        gameId, cubeLine = line.split(":")
+        #print (f"game: {gameId}")
+        #print(f"cubes: {cubeLine}")
+        id = int(gameId.split(" ")[1].strip())
+        cubeGames = cubeLine.split(";")
+        possible = True
+        for game in cubeGames:
+            cubes = game.split(",")
+            for cube in cubes:
+                number, color = cube.strip().split(" ")
+
+                if(cubeList.get(color) != None):
+                    cubeList[color] += int(number)
+                else:
+                    cubeList[color] = int(number)
+            #print(cubeList)
+            possible = check(cubeList.get("red"), cubeList.get("green"), cubeList.get("blue"))
+            #print(possible)
+            cubeList = dict()
+            if(not possible): break
+        print(f"Game possible adding sum?: {possible}")
+        if(possible):
+            
+            sum += id
+        
+        #print("new game")
+print(sum)
+
+        
